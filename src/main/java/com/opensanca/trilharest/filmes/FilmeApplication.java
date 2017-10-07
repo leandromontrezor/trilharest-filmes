46b60294fa0ca984e40774ab372b25c8f93df719
Leandro g <ltrezor@hotmail.com>
2017-10-07 16:12:51 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/opensanca/trilharest/filmes/FilmeApplication.java
@@ -0,0 +1,16 @@
+package com.opensanca.trilharest.filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class FilmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmeApplication.class, args);
+    }
+
+
+}
